4af30f066d3263732264f1b14d4669bdd3441b65
Milen Stanev <milen.stanev@gmail.com>
2017-06-17 18:47:12 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'angular-ui/ui-router';
+import angular from 'angular/bower-angular';
+
+export default angular;
---
+++ util/dist.js
@@ -0,0 +1,49 @@
+"use strict";
+const fs = require('fs');
+const Builder = require('systemjs-builder');
+
+const pjson = require('../package.json');
+
+let builderConfigMeta = {};
+/*for(let key in pjson.jspm.dependencies) {
+  builderConfigMeta[key] = {
+    build: false
+  };
+}*/
+
+/**
+ * Define baseUrl
+ * . -> build, run as npm script file
+ * .. -> debug, run as node file
+ * @type {string}
+ */
+const baseUrl = '.';
+/**
+ * Configure builder paths
+ */
+const builder = new Builder(`${baseUrl}/`, `${baseUrl}/config.js`);
+
+builder.config({
+  meta: builderConfigMeta
+});
+
+
+builder
+  .buildStatic(
+    `${baseUrl}/index.js`,
+    `${baseUrl}/dist/index.dist.js`,
+    {
+      inject: true,
+      minify: false,
+      mangle: false,
+      sourceMaps: true,
+      format: 'umd',
+      runtime: false
+    }
+  ).then(function() {
+  console.log('Build complete\n');
+})
+  .catch(function(err) {
+    console.log('Build error\n');
+    console.log(err);
+  });
